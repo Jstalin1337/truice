9b604ed49538a954542c2b03047617665d07e48b
Ishigure <Ishigure@localhost>
2010-09-16 15:29:50 +0200

    Update useSpells to TDB 34 & edit Spell Indexes

--- Functions.pas
+++ Functions.pas
@@ -56,8 +56,8 @@ const
   I_Map                  =  4;
   I_ItemPetFood          =  1;
   I_AreaTable            = 11;
-  I_SpellName            = 142;
-  I_SpellRank            = 159;
+  I_SpellName            = 141;
+  I_SpellRank            = 158;
 
   MAX_ITEM_LENGTH        = 1000;
 
